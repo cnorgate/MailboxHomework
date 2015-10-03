6cd6c270c012b37b498bd55d1f9ff09f42654325
cnorgate <cnorgate@tcc-cnorgate2.local>
2015-10-02 17:14:22 -0700

    Got reschedule screen to show - going to get it to hide next

--- MailboxHomework/MailboxViewController.swift
+++ MailboxHomework/MailboxViewController.swift
@@ -17,10 +17,35 @@ class MailboxViewController: UIViewController {
     @IBOutlet weak var nav: UIImageView!
     
     @IBOutlet weak var message: UIImageView!
+    var messageOriginalCenter: CGPoint!
+    
     
     @IBOutlet weak var feed: UIImageView!
     
     @IBOutlet weak var search: UIImageView!
+    
+    @IBOutlet weak var laterIcon: UIImageView!
+    var laterOriginalCenter: CGPoint!
+    
+    @IBOutlet weak var archiveIcon: UIImageView!
+    var archiveOriginalCenter: CGPoint!
+    
+    @IBOutlet weak var containerView: UIView!
+    
+    @IBOutlet weak var backgroundView: UIImageView!
+    
+    @IBOutlet weak var reschedule: UIImageView!
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
     
@@ -31,6 +56,10 @@ class MailboxViewController: UIViewController {
         
         //scrollView.contentSize = CGSize(width: 320, height: 1000)
         scrollView.contentSize = CGSize(width: 320, height: message.center.y + feed.center.y + 560)
+        
+        backgroundView.backgroundColor = UIColor.grayColor()
+
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -40,9 +69,105 @@ class MailboxViewController: UIViewController {
     
     @IBAction func onPanMessage(sender: UIPanGestureRecognizer) {
         
+        var point = sender.locationInView(view)
+        var velocity = sender.velocityInView(view)
+        var translation = sender.translationInView(view)
         
-        
+        if sender.state == UIGestureRecognizerState.Began {
+            print("Gesture began at: \(point)")
+            //Store original point of message
+            messageOriginalCenter = message.center
+            laterOriginalCenter = laterIcon.center
+            archiveOriginalCenter = archiveIcon.center
+        } else if sender.state == UIGestureRecognizerState.Changed {
+            //Edit the trays location
+            message.center = CGPoint(x: messageOriginalCenter.x + translation.x, y: messageOriginalCenter.y)
+            
+            
+            // Adjust the opacity of the icon as scrolling continues
+            if translation.x < -30 {
+                UIView.animateWithDuration(0.5, delay: 0, usingSpringWithDamping: 0, initialSpringVelocity: 0, options: [], animations: { () -> Void in
+                    self.laterIcon.alpha = 1
+                    }, completion: { (completed) -> Void in
+                        //nothing
+                })
+            } else {
+                UIView.animateWithDuration(0.5, delay: 0, usingSpringWithDamping: 0, initialSpringVelocity: 0, options: [], animations: { () -> Void in
+                    self.laterIcon.alpha = 0.5
+                    }, completion: { (completed) -> Void in
+                        //nothing
+                })
+            }
+            
+            
+            
+            // Start moving the icon, and change the color background of the view
+            if translation.x < -60{
+                laterIcon.center = CGPoint(x: laterOriginalCenter.x + translation.x + 60, y: laterOriginalCenter.y)
+                backgroundView.backgroundColor = UIColor.orangeColor()
+                //(red: , green: 255, blue: 102, alpha: 1.0)
+            } else {
+                laterIcon.center = CGPoint(x: laterOriginalCenter.x, y: laterOriginalCenter.y)
+                backgroundView.backgroundColor = UIColor.grayColor()
+            }
+            
+            
+            
+
+            print("Gesture changed at: \(point) \(translation)")
+        } else if sender.state == UIGestureRecognizerState.Ended {
+            
+            // Snap the message back to its original position
+            if translation.x < -260 {
+                
+
+            } else if translation.x < -60 {
+                archiveIcon.alpha = 0
+                
+                
+                
+                UIView.animateWithDuration(0.5, delay: 0, options: [], animations: { () -> Void in
+                    // Move message back to original place
+                    self.message.center = CGPoint(x: self.messageOriginalCenter.x - 320 , y: self.messageOriginalCenter.y)
+                    
+                    // Move the icons back to original positions
+                    self.laterIcon.center = CGPoint(x: self.laterOriginalCenter.x - 320, y: self.laterOriginalCenter.y)
+                    
+                    self.reschedule.alpha = 1
+                    
+                    print("Gesture ended at: \(point)")
+                    }, completion: { (completed) -> Void in
+                        //nothing
+                })
+                
+            } else {
+                UIView.animateWithDuration(0.5, delay: 0, options: [], animations: { () -> Void in
+                    // Move message back to original place
+                    self.message.center = CGPoint(x: self.messageOriginalCenter.x, y: self.messageOriginalCenter.y)
+                    
+                    // Move the icons back to original positions
+                    self.laterIcon.center = CGPoint(x: self.laterOriginalCenter.x, y: self.laterOriginalCenter.y)
+                    
+                    print("Gesture ended at: \(point)")
+                    }, completion: { (completed) -> Void in
+                        //nothing
+                })
+            }
+        }
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
     /*
     // MARK: - Navigation
