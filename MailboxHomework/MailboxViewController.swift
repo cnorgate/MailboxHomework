0ccb35a0bafe7f9d2f29f284e757f02879e73aa4
cnorgate <cnorgate@tcc-cnorgate2.local>
2015-10-06 10:47:11 -0700

    Just before implementing the last list page for the first part of app

--- MailboxHomework/MailboxViewController.swift
+++ MailboxHomework/MailboxViewController.swift
@@ -28,6 +28,12 @@ class MailboxViewController: UIViewController {
     @IBOutlet weak var laterIcon: UIImageView!
     var laterOriginalCenter: CGPoint!
     
+    @IBOutlet weak var listIcon: UIImageView!
+    
+    
+    @IBOutlet weak var laterIconWrapper: UIView!
+    var laterWrapperOriginalCenter: CGPoint!
+    
     @IBOutlet weak var archiveIcon: UIImageView!
     var archiveOriginalCenter: CGPoint!
     
@@ -37,6 +43,7 @@ class MailboxViewController: UIViewController {
     
     @IBOutlet weak var reschedule: UIImageView!
     
+    @IBOutlet weak var listScreen: UIImageView!
     
     
     
@@ -68,72 +75,104 @@ class MailboxViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
+    
+    
     @IBAction func onPanMessage(sender: UIPanGestureRecognizer) {
         
+        // Set points and velocity
         var point = sender.locationInView(view)
         var velocity = sender.velocityInView(view)
         var translation = sender.translationInView(view)
         
+        
+        
+        
+        // Pan begin
         if sender.state == UIGestureRecognizerState.Began {
             print("Gesture began at: \(point)")
+            
             //Store original point of message
             messageOriginalCenter = message.center
             laterOriginalCenter = laterIcon.center
+            laterWrapperOriginalCenter = laterIconWrapper.center
             archiveOriginalCenter = archiveIcon.center
             feedOriginalCenter = feed.center
+        
+            
+            
+            
+        // Pan Change
         } else if sender.state == UIGestureRecognizerState.Changed {
             //Edit the trays location
             message.center = CGPoint(x: messageOriginalCenter.x + translation.x, y: messageOriginalCenter.y)
             
-            
-            // Adjust the opacity of the icon as scrolling continues
-            if translation.x < -30 {
+            // Change icons and background color
+            if translation.x < -260{
+                //laterIcon.center = CGPoint(x: laterOriginalCenter.x + translation.x + 60, y: laterOriginalCenter.y)
+                laterIconWrapper.center = CGPoint(x: laterWrapperOriginalCenter.x + translation.x + 60, y: laterWrapperOriginalCenter.y)
+                backgroundView.backgroundColor = UIColor.brownColor()
+                laterIcon.alpha = 0
+                listIcon.alpha = 1
+                //(red: , green: 255, blue: 102, alpha: 1.0)
+            } else if translation.x < -60{
+                //laterIcon.center = CGPoint(x: laterOriginalCenter.x + translation.x + 60, y: laterOriginalCenter.y)
+                laterIconWrapper.center = CGPoint(x: laterWrapperOriginalCenter.x + translation.x + 60, y: laterWrapperOriginalCenter.y)
+                backgroundView.backgroundColor = UIColor.orangeColor()
+                laterIcon.alpha = 1
+                listIcon.alpha = 0
+                //(red: , green: 255, blue: 102, alpha: 1.0)
+            } else if translation.x < -30 {
                 UIView.animateWithDuration(0.5, delay: 0, usingSpringWithDamping: 0, initialSpringVelocity: 0, options: [], animations: { () -> Void in
                     self.laterIcon.alpha = 1
                     }, completion: { (completed) -> Void in
                         //nothing
                 })
+                backgroundView.backgroundColor = UIColor.grayColor()
             } else {
+                laterIconWrapper.center = CGPoint(x: laterWrapperOriginalCenter.x, y: laterWrapperOriginalCenter.y)
                 UIView.animateWithDuration(0.5, delay: 0, usingSpringWithDamping: 0, initialSpringVelocity: 0, options: [], animations: { () -> Void in
                     self.laterIcon.alpha = 0.5
                     }, completion: { (completed) -> Void in
                         //nothing
                 })
-            }
-            
-            
-            
-            // Start moving the icon, and change the color background of the view
-            if translation.x < -60{
-                laterIcon.center = CGPoint(x: laterOriginalCenter.x + translation.x + 60, y: laterOriginalCenter.y)
-                backgroundView.backgroundColor = UIColor.orangeColor()
-                //(red: , green: 255, blue: 102, alpha: 1.0)
-            } else {
-                laterIcon.center = CGPoint(x: laterOriginalCenter.x, y: laterOriginalCenter.y)
-                backgroundView.backgroundColor = UIColor.grayColor()
-            }
-            
-            
-            
 
+                //backgroundView.backgroundColor = UIColor.grayColor()
+            }
             print("Gesture changed at: \(point) \(translation)")
+            
+            
+         
+            
+        // Pan ended
         } else if sender.state == UIGestureRecognizerState.Ended {
             
             // Snap the message back to its original position
             if translation.x < -260 {
-                
-
-            } else if translation.x < -60 {
                 archiveIcon.alpha = 0
-                
-                
                 
                 UIView.animateWithDuration(0.5, delay: 0, options: [], animations: { () -> Void in
                     // Move message back to original place
                     self.message.center = CGPoint(x: self.messageOriginalCenter.x - 320 , y: self.messageOriginalCenter.y)
                     
                     // Move the icons back to original positions
-                    self.laterIcon.center = CGPoint(x: self.laterOriginalCenter.x - 320, y: self.laterOriginalCenter.y)
+                    self.laterIconWrapper.center = CGPoint(x: self.laterWrapperOriginalCenter.x - 320, y: self.laterWrapperOriginalCenter.y)
+                    
+                    self.listScreen.alpha = 1
+                    
+                    print("Gesture ended at: \(point)")
+                    }, completion: { (completed) -> Void in
+                        //nothing
+                })
+            } else if translation.x < -60 {
+                archiveIcon.alpha = 0
+                
+                UIView.animateWithDuration(0.5, delay: 0, options: [], animations: { () -> Void in
+                    // Move message back to original place
+                    self.message.center = CGPoint(x: self.messageOriginalCenter.x - 320 , y: self.messageOriginalCenter.y)
+                    
+                    // Move the icons back to original positions
+                    self.laterIconWrapper.center = CGPoint(x: self.laterWrapperOriginalCenter.x - 320, y: self.laterWrapperOriginalCenter.y)
                     
                     self.reschedule.alpha = 1
                     
@@ -148,7 +187,7 @@ class MailboxViewController: UIViewController {
                     self.message.center = CGPoint(x: self.messageOriginalCenter.x, y: self.messageOriginalCenter.y)
                     
                     // Move the icons back to original positions
-                    self.laterIcon.center = CGPoint(x: self.laterOriginalCenter.x, y: self.laterOriginalCenter.y)
+                    self.laterIconWrapper.center = CGPoint(x: self.laterWrapperOriginalCenter.x, y: self.laterWrapperOriginalCenter.y)
                     
                     print("Gesture ended at: \(point)")
                     }, completion: { (completed) -> Void in
@@ -168,7 +207,7 @@ class MailboxViewController: UIViewController {
     
     
     
-    
+    // Tap out of the reschedule
     @IBAction func onTapReschedule(sender: UITapGestureRecognizer) {
         print("tapped")
         
@@ -196,7 +235,7 @@ class MailboxViewController: UIViewController {
             self.message.center = CGPoint(x: self.messageOriginalCenter.x, y: self.messageOriginalCenter.y)
             
             // Move the icons back to original positions
-            self.laterIcon.center = CGPoint(x: self.laterOriginalCenter.x, y: self.laterOriginalCenter.y)
+            self.laterIconWrapper.center = CGPoint(x: self.laterWrapperOriginalCenter.x, y: self.laterWrapperOriginalCenter.y)
             }, completion: { (completed) -> Void in
                 //nothing
         })
